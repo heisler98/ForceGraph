e7dac42eff1077f720421c83339e208e5ba5e5bf
Hunter Eisler <heisler98@gmail.com>
2020-06-29 01:20:45 -0500

    Initial Commit

---
+++ ForceGraph/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ForceGraph
+//
+//  Created by Hunter Eisler on 6/29/20.
+//  Copyright © 2020 Hunter Eisler. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
